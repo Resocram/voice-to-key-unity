03ae7d48b3b6ce8c5c2c390216e2e98fad9ccea1
Kevin Chin <kevin.chin.cs@gmail.com>
2019-11-30 15:40:38 -0800

    move and obstacles

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject obstacle;
+    private int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter >= 120) {
+
+            Instantiate(obstacle, new Vector3(1, 1, 1), Quaternion.identity);
+
+            Debug.Log("spawn obstacles");
+            counter = 0; //reset counter to 0
+        }
+
+
+        counter += 1;
+    }
+}
