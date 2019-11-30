e8d9616939acd2591fd1b04feeb2b4d24459cd08
Kevin Chin <kevin.chin.cs@gmail.com>
2019-11-30 14:51:59 -0800

    first commit

---
+++ Assets/Scripts/VoiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceTest : MonoBehaviour
+{
+
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("started");
+        actions.Add("forward", Forward);
+        actions.Add("up", Forward);
+        actions.Add("down", Forward);
+        actions.Add("left", Forward);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech) {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward() {
+        transform.Translate(1, 0, 0);
+    }
+
+}
